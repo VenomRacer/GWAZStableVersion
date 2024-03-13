ea171b33a1b5ba93b72fb4d3f1f1350c7547e571
VenomRacer <venomracer24@gmail.com>
2024-03-13 08:44:40 +0800

    2

--- app/src/main/java/com/av/avmessenger/Messaging/M1/m2/MainActivity.java
+++ app/src/main/java/com/av/avmessenger/Messaging/M1/m2/MainActivity.java
@@ -68,7 +68,7 @@ public class MainActivity extends AppCompatActivity{
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                //bakit
+
 
             }
         });
