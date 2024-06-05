7ecb7ffbb082583dc00e42206e15b1a517f50c3b
VenomRacer <venomracer24@gmail.com>
2024-06-05 09:46:07 +0800

    June 5,2024

--- app/src/main/java/com/av/Gwaz/login/splash.java
+++ app/src/main/java/com/av/Gwaz/login/splash.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -13,12 +14,14 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.av.Gwaz.R;
+import com.facebook.shimmer.ShimmerFrameLayout;
 
 public class splash extends AppCompatActivity {
-    ImageView logo;
+    ImageView logo, gwaztxt;
     TextView name, own1, own2;
-    Animation topAnim, bottomAnim;
+    Animation topAnim, bottomAnim, logoAnimSet;
     MediaPlayer mediaPlayer;
+    private ShimmerFrameLayout shimmerFrameLayout;
 
 
 
@@ -27,21 +30,51 @@ public class splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        shimmerFrameLayout = findViewById(R.id.shimmer_view_container);
+
         logo = findViewById(R.id.logoimg);
+        gwaztxt = findViewById(R.id.gwaztxt);
         own1 = findViewById(R.id.ownone);
         own2 = findViewById(R.id.owntwo);
 
         topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        logoAnimSet = AnimationUtils.loadAnimation(this, R.anim.logo_animation_set);
 
 
-        logo.setAnimation(topAnim);
+        logo.setAnimation(logoAnimSet);
         own1.setAnimation(bottomAnim);
         own2.setAnimation(bottomAnim);
 
-        mediaPlayer = MediaPlayer.create(this, R.raw.gchord);
+
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.intro_track);
         mediaPlayer.start();
 
-      new Handler().postDelayed(new Runnable() {
+        // Start the shimmer effect after a delay of 2.5 seconds (2500 milliseconds)
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                shimmerFrameLayout.setVisibility(View.VISIBLE);
+                shimmerFrameLayout.startShimmer();
+
+                // Stop the shimmer effect after one cycle (based on shimmer_duration)
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        shimmerFrameLayout.stopShimmer();
+                        gwaztxt.setVisibility(View.VISIBLE);
+                    }
+                }, 1000); // Adjust this delay to match the shimmer_duration
+            }
+        }, 2500);
+
+
+
+
+
+        new Handler().postDelayed(new Runnable() {
           @Override
           public void run() {
               Intent intent = new Intent(splash.this, login.class);
@@ -60,4 +93,12 @@ public class splash extends AppCompatActivity {
             mediaPlayer = null;
         }
     }
+    @Override
+    protected void onPause() {
+        shimmerFrameLayout.stopShimmer();
+        super.onPause();
+    }
+
+
+
 }
